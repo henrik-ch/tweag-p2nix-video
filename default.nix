a07e3d4e49a42a4dacc7a9916dffc5310167a9f8
i97henka <i97henka@gmail.com>
2023-05-24 07:33:23 +0100

    pinned nixpkgs to 22.11 - no improvement

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") {} }:
   pkgs.poetry2nix.mkPoetryApplication {
     projectDir = ./.;
   }
