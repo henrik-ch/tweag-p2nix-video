c051cab2a77d2ddb6a7962bca8ecfdbfcc98f438
i97henka <i97henka@gmail.com>
2023-05-24 09:07:10 +0100

    failed attempt to add override as suggested in edge cases md

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.poetry2nix.mkPoetryApplication {
     projectDir = ./.;
+    overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend 
+    (self: super: {
+      # Add your overrides here.
+      weburl = super.weburl.overrideAttrs 
+      (
+        old: {
+        buildInputs = (old.buildInputs or [ ])  ++ [ super.hatchling ];
+      }
+      );
+    });
   }
